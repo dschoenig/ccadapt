3c9b4f704dc041abaa5a4ef56f426bfa6d8ff85c
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-08-16 09:14:21 -0400

    2PL model

---
+++ src/1_bayes_2.R
@@ -0,0 +1,69 @@
+library(data.table)
+library(stringi)
+library(brms)
+
+source("paths.R")
+source("utilities.R")
+
+
+survey.irt <- readRDS(file.survey.irt)
+variables <- readRDS(file.variables.proc)
+dependencies <- readRDS(file.questions.dependencies)
+
+vars.pred <- variables[code %in% names(survey.irt), code]
+
+
+## ITEM-RESPONSE MODEL 2PL ############################################
+
+
+form.eta <- 
+  paste0("eta ~ ",
+         paste0(vars.pred, collapse = " + "),
+         " + (1 + ", paste0(vars.pred, collapse = " + "), " |i| item)",
+         " + (1 | id)") |>
+  as.formula()
+
+
+form.logalpha <- logalpha ~ 1 + (1 |i| item)
+
+# form.logalpha <- 
+#   paste0("logalpha ~ ",
+#          paste0(vars.pred, collapse = " + "),
+#          " + (1 + ", paste0(vars.pred, collapse = " + "), " |i| item)") |>
+#   as.formula()
+
+form.imp.2pl <-
+  bf(resp ~ exp(logalpha) * eta,
+     form.eta,
+     form.logalpha,
+     nl = TRUE)
+
+prior.imp.2pl <-
+  prior("normal(0, 5)", class = "b", nlpar = "eta") +
+  prior("normal(0, 1)", class = "b", nlpar = "logalpha") +
+  prior("normal(0, 3)", class = "sd", group = "id", nlpar = "eta") +
+  prior("normal(0, 3)", class = "sd", group = "item", nlpar = "eta") +
+  prior("normal(0, 1)", class = "sd", group = "item", nlpar = "logalpha")
+
+get_prior(form.imp.2pl,
+          data = survey.irt,
+          family = brmsfamily("bernoulli", "logit"))
+
+mod.imp.2pl <-
+  brm(formula = form.imp.2pl,
+      data = survey.irt,
+      family = brmsfamily("bernoulli", "logit"),
+      init = 0,
+      silent = 0,
+      chains = 4,
+      cores = 4,
+      threads = 8,
+      warmup = 2500,
+      iter = 5000,
+      refresh = 25,
+      empty = TRUE,
+      prior = prior.imp.2pl)
+
+
+saveRDS(mod.imp.2pl, paste0(path.results.brm, "mod.imp.2pl.rds"))
+
