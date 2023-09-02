1d04c0c9156678ec39d58d82d9b82523caef6138
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-09-02 07:31:23 -0400

    Actually trying a full Bayesian model

---
+++ src/1_irt_2pl_hs.R
@@ -0,0 +1,116 @@
+library(data.table)
+library(stringi)
+library(brms)
+
+source("paths.R")
+source("utilities.R")
+
+
+survey.fit <- readRDS(file.survey.rf)
+variables <- readRDS(file.variables.proc)
+dependencies <- readRDS(file.questions.dependencies)
+
+
+
+# Remove ordering from factor variables
+var.ord <- names(which(unlist(lapply(survey.fit, is.ordered))))
+survey.fit[, (var.ord) := lapply(.SD,
+                                 \(x) factor(x,
+                                             ordered = FALSE,
+                                             levels = levels(x))),
+           .SDcols = var.ord]
+
+survey.irt <- copy(survey.fit)
+
+var.cat <- variables[code %in% names(survey.irt) &
+                     type == "categorical" &
+                     category.adaptation != 1,
+                     .(code, cat.ref)]
+for(i in 1:nrow(var.cat)) {
+  survey.irt[[var.cat$code[i]]] <-
+    relevel(survey.irt[[var.cat$code[i]]], var.cat$cat.ref[i])
+}
+
+var.cont <- variables[code %in% names(survey.irt) &
+                      type == "continuous" &
+                      category.adaptation != 1,
+                      .(code, cont.mean, cont.sd)]
+# var.cont <- var.sel[type == "continuous", .(code, cont.mean, cont.sd)]
+if(nrow(var.cont) > 0) {
+  for(i in 1:nrow(var.cont)) {
+    survey.irt[[var.cont$code[i]]] <-
+      (survey.irt[[var.cont$code[i]]] - var.cont$cont.mean[i]) /
+      var.cont$cont.sd[i]
+  }
+}
+
+survey.irt <- copy(survey.irt)
+
+survey.irt[, id := 1:.N]
+
+
+sel.adaptation <- variables[main == "mgmtChangeACC", code]
+items <- c(paste0("y.acc.", 1:length(sel.adaptation)))
+
+survey.irt <-
+  melt(copy(survey.irt),
+       measure.vars = items,
+       variable.name = "item",
+       value.name = "resp")
+
+
+# Fix names to avoid brms error
+setnames(survey.irt, stri_replace_first_regex(names(survey.irt), "^(\\w)(\\d)$", "$10$2"))
+vars.pred.cat <- stri_replace_first_regex(vars.pred.cat, "^(\\w)(\\d)$", "$10$2")
+vars.pred.cont <- stri_replace_first_regex(vars.pred.cont, "^(\\w)(\\d)$", "$10$2")
+
+
+
+## ITEM-RESPONSE MODEL 2PL ############################################
+
+
+form.eta <- 
+  paste0("eta ~ ",
+         paste0(c(vars.pred.cont, vars.pred.cat), collapse = " + "),
+         " + (1 + ", paste0(c(vars.pred.cont, vars.pred.cat), collapse = " + "), " |i| item)",
+         " + (1 | id)") |>
+  as.formula()
+
+
+prior.imp.2pl <-
+  prior(horseshoe(df = 3, par_ratio = 0.1), class = "b", nlpar = "eta") +
+  prior("normal(0, 1)", class = "b", nlpar = "logalpha") +
+  prior("normal(0, 3)", class = "sd", group = "id", nlpar = "eta") +
+  prior("normal(0, 3)", class = "sd", group = "item", nlpar = "eta") +
+  prior("normal(0, 1)", class = "sd", group = "item", nlpar = "logalpha")
+
+
+
+form.logalpha <- logalpha ~ 1 + (1 |i| item)
+
+form.imp.2pl <-
+  bf(resp ~ exp(logalpha) * eta,
+     form.eta,
+     form.logalpha,
+     nl = TRUE)
+
+
+mod.imp.2pl <-
+  brm(formula = form.imp.2pl,
+      data = survey.irt,
+      family = brmsfamily("bernoulli", "logit"),
+      init = 0,
+      silent = 0,
+      chains = 4,
+      cores = 4,
+      threads = 8,
+      warmup = 2500,
+      iter = 5000,
+      refresh = 25,
+      prior = prior.imp.2pl)
+
+
+dir.create(path.results.irt, recursive = TRUE, showWarnings = FALSE)
+
+saveRDS(mod.imp.2pl, file.irt.mod.2pl.nl)
+
---
+++ src/cc_1_irt_2pl_hs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=24G
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=2pl_hs
+
+module load StdEnv/2020 gcc/9.3.0 r/4.2.2
+
+Rscript 1_irt_2pl_hs.R
