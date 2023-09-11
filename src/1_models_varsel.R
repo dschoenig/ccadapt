8e3f3b903b20db30e45c3362d3c1f1a7c71c48c9
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-09-11 13:38:40 -0400

    Fix typo

--- src/1_models_varsel.R
+++ src/1_models_varsel.R
@@ -13,7 +13,7 @@ source("utilities.R")
 
 mod.id <- as.integer(args[1])
 resp.type <- as.character(args[2])
-n.threads <- as.character(args[3])
+n.threads <- as.numeric(args[3])
 # mod.id <- 1
 # resp.type <- "urgency"
 # resp.type <- "willingness"
