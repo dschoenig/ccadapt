78572e1a85d5cb3d591ce2f6f93fa7b0cdeae5d7
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-09-14 14:14:41 -0400

    Categorical on cluster

--- src/cc_1_cv_varsel_c_1.sh
+++ src/cc_1_cv_varsel_c_1.sh
@@ -3,11 +3,10 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=96G
 #SBATCH --time=48:00:00
-#SBATCH --array=1-10
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=cv_c
 
 module load StdEnv/2020 gcc/9.3.0 r/4.2.2
 
-Rscript 1_cv_varsel.R $SLURM_ARRAY_TASK_ID categorical 8
+Rscript 1_cv_varsel.R 1 categorical 8
--- src/cc_1_models_varsel_c.sh
+++ src/cc_1_models_varsel_c.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=48G
-#SBATCH --time=24:00:00
+#SBATCH --mem=24G
+#SBATCH --time=6:00:00
 #SBATCH --array=1-10
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
