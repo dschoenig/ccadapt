be71a118d5cd625aa3eb9787bb73f50415d16283
Daniel Schoenig <daniel.schoenig@protonmail.com>
2023-12-07 11:46:00 -0500

    Resource allocation

--- src/cc_1_models_varsel_w.sh
+++ src/cc_1_models_varsel_w.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=48G
-#SBATCH --time=4:00:00
+#SBATCH --mem=24G
+#SBATCH --time=3:00:00
 #SBATCH --array=1-12
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
@@ -10,4 +10,4 @@
 
 module load StdEnv/2020 gcc/9.3.0 r/4.2.2
 
-Rscript 1_models_varsel.R $SLURM_ARRAY_TASK_ID willingness
+Rscript 1_models_varsel.R $SLURM_ARRAY_TASK_ID willingness 4
