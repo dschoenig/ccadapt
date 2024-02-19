eba6253d1003d0f4df049b0f0d0a2dcf7cc4a012
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-02-19 11:28:46 -0500

    Fix comments

--- src/1_models_varsel.R
+++ src/1_models_varsel.R
@@ -11,9 +11,9 @@ library(doRNG)
 source("paths.R")
 source("utilities.R")
 
-# mod.id <- as.integer(args[1])
-# resp.type <- as.character(args[2])
-# n.threads <- as.numeric(args[3])
+mod.id <- as.integer(args[1])
+resp.type <- as.character(args[2])
+n.threads <- as.numeric(args[3])
 
 # mod.id <- 1
 # resp.type <- "willingness"
